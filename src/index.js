b0baf55d5514bc2a5ae8f9cc78c6bad5d27d784d
Alexandre Corazza Marcolino <alexandre_corazza@yahoo.com.br>
2024-01-30 22:32:45 -0300

    refactoring and add fourth test

--- src/index.js
+++ src/index.js
@@ -1,5 +1,12 @@
-const sum = () => {
-  return false
+const sum = (list) => {
+  if (!list || list.length === 0 || list.length === 1) return false
+  else {
+    const maxNumber = Math.max(...list)
+    const minNumber = Math.min(...list)
+    const excludeMaxMinNumbers = maxNumber + minNumber;
+
+    return list.reduce((total, number) => total + number, 0) - excludeMaxMinNumbers;
+  }
 };
   
 module.exports = {
--- tests/index.test.js
+++ tests/index.test.js
@@ -5,9 +5,13 @@ test("should return false if an empty value is provided", () => {
 })
 
 test("should return false if the provided array is an empty list", () => {
-    expect(sum()).toBe(false);
+    expect(sum([])).toBe(false);
 })
 
 test("should return false if the provided array has only one element", () => {
-    expect(sum()).toBe(false);
+    expect(sum([10])).toBe(false);
+})
+
+test("should return 16 as a result of summing the list {6, 2, 1, 8, 1}.", () => {
+    expect(sum([6, 2, 1, 8, 10])).toBe(16);
 })
